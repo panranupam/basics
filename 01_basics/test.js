305e328c4c0a1b7bc159743f1c3ad840c24b71cc
panranupam <panranupam@gmail.com>
2025-03-13 11:52:24 +0000

    for test file

---
+++ 01_basics/test.js
@@ -0,0 +1,7 @@
+const accountId = 23456;
+let accountType= "Savings";
+var accountLocation = "Noida";
+accountHolderName =  "Ashok Kumar Singh";
+
+console.log(accountId);
+console.table([accountId, accountLocation, accountType, accountHolderName])
